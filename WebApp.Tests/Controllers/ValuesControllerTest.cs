65f59ad9bbd498fe86888a64ef69cfa2501e3936
dimsa <dimsa@inbox.ru>
2017-11-02 13:38:35 +0700

    Add project files.

---
+++ WebApp.Tests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using WebApp;
+using WebApp.Controllers;
+using Xunit;
+
+namespace WebApp.Tests.Controllers
+{
+    public class ValuesControllerTest
+    {
+        [Fact]
+        public void Get()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            IEnumerable<string> result = controller.Get();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count());
+            Assert.Equal("value1", result.ElementAt(0));
+            Assert.Equal("value2", result.ElementAt(1));
+        }
+
+        [Fact]
+        public void GetById()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            string result = controller.Get(5);
+
+            // Assert
+            Assert.Equal("value", result);
+        }
+
+        [Fact]
+        public void Post()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            controller.Post("value");
+
+            // Assert
+        }
+
+        [Fact]
+        public void Put()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            controller.Put(5, "value");
+
+            // Assert
+        }
+
+        [Fact]        
+        public void Delete()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+
+            // Act
+            controller.Delete(5);
+
+            // Assert
+        }
+    }
+}
